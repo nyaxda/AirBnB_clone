717e2a5da69c403ae3a16ceaff23fd5d0d3575d8
Daniel Nyakundi <nyaxda@gmail.com>
2024-03-09 01:56:20 -0800

    Refactor model classes to use constructor initialization

--- models/city.py
+++ models/city.py
@@ -5,4 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """City class. Inherits from BaseModel."""
-    state_id = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.state_id = ""
--- models/state.py
+++ models/state.py
@@ -5,4 +5,6 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """State class. Inherits from BaseModel."""
-    name = ""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = ""
